cb9c79d44f5ddafe3fcf4a45e2afe8e9c88acf95
Fabian Steuer <faebse@posteo.de>
2022-08-30 08:33:19 +0100

    Add Patientor frontend

--- express-app/index.ts
+++ express-app/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
